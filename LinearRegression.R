3ed7260a5923a95857417e8c7dcce907441e5d9e
Grwat33 <Grwat33@gmail.com>
2021-10-19 11:29:49 -0400

    correlation matrix viz

--- LinearRegression.R
+++ LinearRegression.R
@@ -4,4 +4,8 @@
 cleancar_df <- read.table(file = "C:/Users/garre/OneDrive/Desktop/RegressionModel/Dataset/CleanCarDataset.csv", sep = ",", header=TRUE)
 cleancar_df
 
+# view correlation matrix
 cor(cleancar_df)
+install.packages("corrplot")
+library(corrplot)
+corrplot(cor(cleancar_df))
